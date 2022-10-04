1b085c7ad12003eece28d417b5f392d30fa3505c
ArmndoHg <114115791+ArmndoHg@users.noreply.github.com>
2022-10-04 19:17:18 +0200

    Add files via upload

---
+++ name.js
@@ -0,0 +1,11 @@
+let name = "Armando ";
+let firstName = "Hernandez ";
+let secondName = "Gonzalez";
+let Fullname = name + firstName + secondName;
+
+console.log("Hello, my name is:", Fullname);
+
+let age = 28;
+let current = 2022;
+let birth = current - age;
+console.log("Birth day: ", birth);
